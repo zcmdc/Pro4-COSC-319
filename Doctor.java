c21989aa5b510e25abcafb0bd7a8f219c0ca7407
zcmdc <zcmdc@iup.edu>
2025-04-29 08:47:06 -0400

    Add files via upload

---
+++ Doctor.java
@@ -0,0 +1,10 @@
+
+class Doctor{
+    private int doctorId = 54321;
+    private String doctorPassword = "password";
+
+    public static void main(String[] arg){
+        Doctor doc = new Doctor();
+        System.out.println("DId: " + doc.doctorId + "\nDP: " + doc.doctorPassword + ".");
+    }
+}
